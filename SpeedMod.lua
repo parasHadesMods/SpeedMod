93e42dd20d7f33e0ccd21e13f7c667b9a734557f
paradigmsort <paradigmsort+hades@gmail.com>
2021-02-23 14:07:44 -0800

    in game time toggle for than, plus 'MODDED GAME' warning

--- SpeedMod.lua
+++ SpeedMod.lua
@@ -3,10 +3,18 @@ ModUtil.RegisterMod("SpeedMod")
 --[[ Feature list:
  - 2 sack
  - No tiny vermin
- - No thanatos
+ - No thanatos (unless configured for in-game time)
  - No barge of death
  - No cutscenes
 ]]
+
+local config = {
+  ModName = "Speed Mod",
+  InGameTime = false
+}
+if ModConfigMenu then
+  ModConfigMenu.Register(config)
+end
 
 ModUtil.LoadOnce(function()
   -- no tiny vermin
@@ -31,11 +39,11 @@ ModUtil.WrapBaseFunction("IsEncounterEligible", function( baseFunc, currentRun, 
   if not result then
     return false
   elseif nextEncounterData.Name == "ThanatosTartarus" then
-    return false
+    return config.InGameTime
   elseif nextEncounterData.Name == "ThanatosAsphodel" then
-    return false
+    return config.InGameTime
   elseif nextEncounterData.Name == "ThanatosElysium" then
-    return false
+    return config.InGameTime
   else
     return true
   end
@@ -67,3 +75,56 @@ end)
 ModUtil.BaseOverride("ShowRunIntro", function()
   return
 end)
+
+-- Modded game warning
+function ShowModdedWarning()
+    local obstacleName = "ModdedGame"
+    local text_config_table = DeepCopyTable(UIData.CurrentRunDepth.TextFormat)
+    local x_pos = 1905
+    local y_pos = 90
+
+    -- If this anchor was already created, just modify the existing textbox
+    if ScreenAnchors[obstacleName] ~= nil then
+        ModifyTextBox({
+            Id = ScreenAnchors[obstacleName],
+            Text = text
+        })
+    else -- create a new anchor/textbox and fade it in
+        ScreenAnchors[obstacleName] = CreateScreenObstacle({
+            Name = "BlankObstacle",
+            X = x_pos,
+            Y = y_pos,
+            Group = "Combat_Menu_Overlay"
+        })
+
+        CreateTextBox(
+            MergeTables(
+                text_config_table,
+                {
+                    Id = ScreenAnchors[obstacleName],
+                    Text = "MODDED GAME"
+                }
+            )
+        )
+
+        ModifyTextBox({
+            Id = ScreenAnchors[obstacleName],
+            FadeTarget = 1,
+            FadeDuration = 0.0
+        })
+    end
+end
+
+-- Scripts/RoomManager.lua : 1874
+ModUtil.WrapBaseFunction("StartRoom", function ( baseFunc, currentRun, currentRoom )
+    ShowModdedWarning()
+
+    baseFunc(currentRun, currentRoom)
+end, ShowChamberNumber)
+
+-- Scripts/UIScripts.lua : 145
+ModUtil.WrapBaseFunction("ShowCombatUI", function ( baseFunc, flag )
+    ShowModdedWarning()
+
+    baseFunc(flag)
+end, ShowChamberNumber)
